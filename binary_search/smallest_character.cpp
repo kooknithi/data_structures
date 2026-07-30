6121ea59a343a4ffaaf63913956d61dc7ed8a300
kooknithi <kooknithi007@gmail.com>
2026-07-30 23:07:05 +0530

    second updates

--- binary_search/smallest_character.cpp
+++ binary_search/smallest_character.cpp
@@ -1,5 +1,34 @@
+//smallest character among other characters in an array
 #include<iostream>
 using namespace std;
+int solve_bs(int n,int a[n],int k)
+{
+    int l=0;
+    int r=n-1;
+    int res= -1;
+    while(l<=r)
+    {
+        int mid=l+(r-l)/2; 
+        if(a[mid] == k)
+        {
+            l=mid+1;
+        }
+        else if(a[mid]>k)
+        {
+            res=a[mid];
+            r=mid-1;
+        }
+        else
+        {
+
+        }
+    }
+    if(res== -1)
+    {
+        return -1;
+    }
+    return a[res];
+}
 int main()
 {
     int n;
@@ -14,5 +43,7 @@ int main()
     cout<<"enter a value to get smallest of greatest element:\n";
     int key;
     cin>>key;
-    
+    int val = solve_bs(n,a,k);
+    cout<<"the samllest element greater than "<<k<<" is "<< val;
+    return 0;
 }
