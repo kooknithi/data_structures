59e983baf0feab05de091d7dbb15ec06c6b6e8f9
kooknithi <kooknithi007@gmail.com>
2026-07-30 20:15:16 +0530

    updated

---
+++ binary_search/smallest_character.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array\n";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array elements:\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"enter a value to get smallest of greatest element:\n";
+    int key;
+    cin>>key;
+    
+}
